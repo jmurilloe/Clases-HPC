372f46af4bd2a43a6a62e8e0acd516c71c826695
Jaime Murillo Espitia <jmurilloe@unal.edu.co>
2025-05-10 23:24:22 -0500

    Added sumk correction example using clang and last updates in Homework No 2

--- homework/2_RandomNumbers/random_pdf.cpp
+++ homework/2_RandomNumbers/random_pdf.cpp
@@ -9,10 +9,10 @@ int main(int argc, char **argv)
 {
   const int SEED = std::atoi(argv[1]);
   const int NSAMPLES = std::atoi(argv[2]);
-  const double MU = std::atoi(argv[3]);
-  const double SIGMA = std::atoi(argv[4]);
-  const double XMIN = std::atoi(argv[5]);
-  const double XMAX = std::atoi(argv[6]);
+  const double MU = std::atof(argv[3]);
+  const double SIGMA = std::atof(argv[4]);
+  const double XMIN = std::atof(argv[5]);
+  const double XMAX = std::atof(argv[6]);
   const int NBINS = std::atoi(argv[7]);
 
   compute_pdf(SEED, NSAMPLES, MU, SIGMA, XMIN, XMAX, NBINS);
@@ -24,20 +24,34 @@ void compute_pdf(int seed, int nsamples, double mu, double sigma, double xmin, d
   std::mt19937 gen(seed);
   std::normal_distribution<double> dis{mu, sigma};
   // TODO: histogram stuff
-    // xmin 1 xmax 10 n bins 9   delta 10-1/9 = 1 > xmin += vector con 9 entradas 1 2 3 4 5 6 7 8 9 10
+    // xmin 1 xmax 10 n bins 9   delta 10-1/9 = 1 > xmin += vector con 9 entradas
     // vector (1-2,2-3,3-4,4-5,5-6,6-7,7-8,8-9,9-10)
     //          1   2   3   4   5               9
-
+  double delta = 1.0*(xmax-xmin)/nbins;
+  //std::cout << delta << "delta" << "\n ";
+  std::vector<double> histo (nbins,0);
+  
   for(int n = 0; n < nsamples; ++n) {
     double r = dis(gen);
     // TODO: fill here the counting histogram stuff
-
-    // for (auto x: vector)
-    // if x < r < x+1 toca revisar que el rango eg 1-2 es un label imaginario, en realidad todos son 0
-
-    //      x += 1
+    int index = 0;
+    for (double ii = xmin;ii<xmax;ii+= delta){
+      if ( (ii< r) && (r < ii+delta)){
+        histo[index] += 1; 
+      }
+      index++;
+    
+    }
+    //
 
   }
+  double valuetoplot = (2*xmin+delta)/2.0;
+  for (auto x : histo){ //normalize
+    x = x/nsamples;
+    
+    std::cout << valuetoplot  << "\t " << x << "\n";
+    valuetoplot += delta;
+  } 
   // TODO: compute and print the pdf
 
 }

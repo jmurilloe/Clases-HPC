778e5cdec3e73ebd111e91f87ab7aa582c65f31c
Jaime Murillo Espitia <jmurilloe@unal.edu.co>
2025-05-14 12:47:35 -0500

    Added testing codes and introduction to testing coverage.

---
+++ 2025-05-14-Clase11/catch2_example/factorial.cpp
@@ -0,0 +1,11 @@
+#include "factorial.h"
+#include <iostream>
+int factorial(int number)
+{
+    if (number == 0) return 1;
+    if (number <= 0) {
+        std::cerr << "Negative numbers not allowed. Returning -1\n ";
+        return -1;
+    }
+    return number <= 0 ? number : factorial(number-1)*number;
+}
---
+++ 2025-05-14-Clase11/catch2_example/main_factorial.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "factorial.h"
+
+int main(void)
+{
+    std::cout << factorial(0) << std::endl;
+    std::cout << factorial(2) << std::endl;
+    std::cout << factorial(3) << std::endl;
+    std::cout << factorial(10) << std::endl;
+    return 0;
+}

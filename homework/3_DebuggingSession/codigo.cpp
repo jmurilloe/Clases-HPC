891ea2628b8ee6523e1787f7660078ab81e431c9
Jaime Murillo Espitia <jmurilloe@unal.edu.co>
2025-05-16 12:03:06 -0500

    Added fast exercise.

--- homework/3_DebuggingSession/codigo.cpp
+++ homework/3_DebuggingSession/codigo.cpp
@@ -17,36 +17,40 @@ int main (int argc, char **argv)
  
   baz(25.9);
 
-    const int NX = 2, NY = 3, NZ = 4;
+  const int NX = 2, NY = 3, NZ = 4;
   double *x, y[NY], z[NZ];
   x = new double [NX];
-  int ii, jj, kk;
+  int  kk;
   print_array(x, NX);
   print_array(y, NY);
   print_array(z, NZ);
   std::cout << std::endl;
 
   for (ii = 0; ii < NX; ++ii) {
-    x[jj] = ii;
+    x[ii] = ii; // se pide acceder a la indice -1 de un array
   }
-  print_array(x, NZ);
+  print_array(x, NX); // se pide acceder al indice 2 y 3 de un arreglo que solo tiene 2 elementos
   print_array(y, NY);
-  print_array(z, NZ + NY);
+  print_array(z, NZ);  // se pide acceder al indice 2 y 3 de un arreglo que solo tiene 2 elementos
+                            // acceder a indice 4, 5 y 6 en un arreglo de 4 elementos
   std::cout << std::endl;
   for (jj = 0; jj < NY; ++jj) {
-    x[jj] = ii;
+    x[ii-1] = ii; //se pide rellenar el indice 2 en un arreglo que solo tiene 2 elementos
     y[jj] = jj;
   }
-  print_array(x-NY, NY);
-  print_array(y-NZ, NZ);
-  print_array(z + NZ + NY, NX);
+  //print_array(x-1, 1); // se pide acceder a una parte de la memoria que no corresponde a x
+  //print_array(y-NZ, NZ); // restar dos tipos de variables diferentes
+  //print_array(z + NZ + NY, NX); // sumar dos tipos de variables diferentes
   std::cout << std::endl;
-
+delete[] x;
    return EXIT_SUCCESS;
 }
 
 int foo(int a, int b)
 {
+  if ((a == 0) || (b == 0)){
+    return 0;
+  }
   return a/b + b/bar(a, b) + b/a;
 }
 
@@ -58,9 +62,9 @@ int bar(int a, int b)
 
 double baz(double x)
 {
-  if (x = 0) return;
+  if (x = 0) return 0;
   double v = 1-(x+1);
-  return std::sqrt(x)
+  return std::sqrt(x);
 }
 
 void print_array(const double data[], const int & size)
